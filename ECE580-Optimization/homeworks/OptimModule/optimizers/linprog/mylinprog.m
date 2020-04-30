516ebfb67bf940020ba9abb368f750624ad2ac81
rahuldeshmukh43 <rahul.deshmukh43@gmail.com>
2020-04-29 22:26:33 -0400

    intermediate commit during hw5 ece580, add functinality to linprog to accept LB,UB correctly

--- ECE580-Optimization/homeworks/OptimModule/optimizers/linprog/mylinprog.m
+++ ECE580-Optimization/homeworks/OptimModule/optimizers/linprog/mylinprog.m
@@ -4,12 +4,22 @@ function [x_str, fval] = mylinprog(c,A,b,Aeq,beq,LB,UB,verbose)
 % Given: min c'*x
 % st:    Aeq*x = beq
 %        Ax <= b
-%        x>=0       %%code not generalized to accept LB,UB
+%        0 <= x  or LB <= x <= UB
 if nargin==7
     verbose=0; %by default
 end
-if ~isempty(LB) || ~isempty(UB)
-    error('Not implemented');
+% transform variables: z = x - LB if LB is not 0
+if ~isempty(LB) 
+%     error('Not implemented');
+   b = b - A*LB;
+   if ~isempty(Aeq)
+       beq = beq - Aeq*LB;
+   end
+end
+% stack ineqaulity conditions Iz <= UB-LB
+if ~isempty(UB)
+    A = [A;eye(length(UB))];
+    b = [b; UB-LB];
 end
 %% Convert to std form using slack and surplus variables st b_std>=0
 A_std =[]; b_std = []; c_std =[];
@@ -34,15 +44,20 @@ end
 A1 = [A_std, eye(size(A_std,1))];
 c1 = [zeros(size(A_std,2),1);ones(size(A_std,1),1)];
 basis_idx = size(A_std,2) + (1:size(A_std,1));
-[x_p1, fval_p1, basis_idx_p1, tab]= simplex(A1,b_std,c1, basis_idx, verbose);
+[~, ~, basis_idx_p1, tab]= simplex(A1,b_std,c1, basis_idx, verbose);
 if verbose
    fprintf('\\text{Phase 2:}&\\nonumber\\\\\n%%\n') ;
 end
 % Phase2: Find optimal solution
 A2 = tab(1:end-1,1:size(A_std,2));
 b2 = tab(1:end-1,end);
-[x_str_p2, fval, basis_idx_p2, ~] = simplex(A2, b2, c_std, basis_idx_p1, verbose);
+[x_str_p2, fval,~, ~] = simplex(A2, b2, c_std, basis_idx_p1, verbose);
 x_str = x_str_p2(1:length(c));
+if ~isempty(LB)
+    % transform from z_str to x_str, x_str= z_str + LB
+   x_str = x_str + LB; 
+   fval = fval + c'*LB;
+end
 fprintf('\n** Optimum Solution found using mylinprog **\n');
 if verbose
     display(x_str);
