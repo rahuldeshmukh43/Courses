14c4d3194016f573509c1658a821e5167b58c8ab
rahuldeshmukh43 <rahul.deshmukh43@gmail.com>
2021-04-12 16:33:15 -0400

    changed conv to same mode in lab8 of ece637

--- ECE637-DigitalImageProcessing-I/labs/lab8/code/python/utils.py
+++ ECE637-DigitalImageProcessing-I/labs/lab8/code/python/utils.py
@@ -9,6 +9,22 @@ utility file for image metrics
 import numpy as np
 from scipy.signal import convolve2d
 
+def conv2d(X_img, conv_filter):
+    "apply conv with zero padding"
+    filter_size,_ = conv_filter.shape
+    filter_half_wd = filter_size//2
+    theta = conv_filter.flatten()
+    ht,wd = X_img.shape
+    padded_img = np.zeros((ht+2*filter_half_wd, wd+2*filter_half_wd))
+    padded_img[filter_half_wd:-filter_half_wd, filter_half_wd:-filter_half_wd] = X_img
+    out_img = np.zeros((ht,wd))
+    for row in range(ht):
+        for col in range(wd):
+            x = padded_img[row : row + 2*filter_half_wd + 1, 
+                           col : col + 2*filter_half_wd + 1]
+            out_img[row,col] = np.dot(x.flatten(), theta)
+    return out_img
+
 def Un_gamma_correct(img, gamma):
     lin =  255.*((img/255.)**gamma)
     return lin.astype(np.uint8)
@@ -24,10 +40,11 @@ def LPF(img, sigma=np.sqrt(2), size=7):
         for c in range(size):
             i = r - (size//2)
             j = c - (size//2)
-            h[r,c] = np.exp(-1.0*(i**2+j**2)/(2*sigma**2))
+            h[r,c] = np.exp(-1.0*(i**2+j**2)/(2.0*(sigma**2)))
     h /= np.sum(h) #normalize
     #convolve filter
-    out_img = convolve2d(img, h)
+    out_img = convolve2d(img, h, mode='same')
+    #out_img = conv2d(img, h)
     #rescale to 0-255
     out_img -= out_img.min()
     out_img *= (255./out_img.max())
@@ -36,7 +53,9 @@ def LPF(img, sigma=np.sqrt(2), size=7):
 def RMSE(original_img, binary_image):
     "compute rmse error betweeen two images"
     h,w = original_img.shape
-    rmse = (1./(h*w))*np.sum((original_img.astype(np.float32) - binary_image.astype(np.float32))**2)
+    assert (h,w)== binary_image.shape
+    rmse = (1./(h*w))*np.sum((original_img.astype(np.float32) -
+                              binary_image.astype(np.float32))**2)
     return np.sqrt(rmse)
 
 def Fidelity(original_img, binary_img):
@@ -45,9 +64,11 @@ def Fidelity(original_img, binary_img):
     # un-gamma correct the images
     original_lin = Un_gamma_correct(original_img, gamma)    
     binary_lin = Un_gamma_correct(binary_img, gamma)    
+    #print(np.linalg.norm(binary_lin-binary_img))
     # LPF
     original_lin_lpf = LPF(original_lin)
     binary_lin_lpf = LPF(binary_lin)
+    assert original_lin_lpf.shape==original_img.shape
     # scale to equal brightness (cube-root)
     f_tilde = Scale_to_equal_brightness(original_lin_lpf)
     b_tilde = Scale_to_equal_brightness(binary_lin_lpf)
